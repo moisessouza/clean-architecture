acc807afefecdf4fdf987f5da1ca7f7ba031d26d
Moisés Souza <moi0983@gmail.com>
2022-11-04 13:59:45 -0300

    ajustes na arquitetura

--- web-controller/src/main/java/com/workers/controllers/AnyController.java
+++ web-controller/src/main/java/com/workers/controllers/AnyController.java
@@ -1,24 +1,28 @@
-package com.workers.restservice;
+package com.workers.controllers;
 
 import com.workers.presenters.models.AnyInputImpl;
 import com.workers.presenters.models.AnyOutput;
 import com.workers.usecase.AnyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AnyController {
 
     @Autowired
     private AnyService anyService;
 
     @GetMapping("/any")
-    public AnyOutput getAny() {
+    public String getAny(Model model) {
         AnyOutput anyOutput = anyService.executeSubstringForBusinessRules();
-        return anyOutput;
+
+        model.addAttribute("any", anyOutput);
+
+        return "any";
     }
 
     @PostMapping("/any")
