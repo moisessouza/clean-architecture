a48de13ed54b899b977ffe51f0da7e906045b4df
Moisés Souza <moi0983@gmail.com>
2022-11-07 13:08:45 -0300

    ajustes na arquitetura

--- web-controller/src/main/java/com/workers/controllers/UserController.java
+++ web-controller/src/main/java/com/workers/controllers/UserController.java
@@ -2,16 +2,11 @@ package com.workers.controllers;
 
 import com.workers.models.UserPostModel;
 import com.workers.presenters.models.UserInputImpl;
-import com.workers.presenters.models.UserOutput;
 import com.workers.presenters.models.UserOutputImpl;
 import com.workers.usecase.UserService;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
