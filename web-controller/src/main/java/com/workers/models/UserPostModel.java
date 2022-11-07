645845a20369dc330c37868ca6a40b2d450d92e5
Moisés Souza <moi0983@gmail.com>
2022-11-07 10:38:45 -0300

    ajustes na arquitetura

--- web-controller/src/main/java/com/workers/models/UserPostModel.java
+++ web-controller/src/main/java/com/workers/models/UserPostModel.java
@@ -1,23 +1,11 @@
 package com.workers.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserPostModel {
     private String email;
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
