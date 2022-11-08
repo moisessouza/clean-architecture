b6a7a9cea4f348d3ede272d24ed79b506bb8c74d
Moisés Souza <moi0983@gmail.com>
2022-11-08 10:49:53 -0300

    ajustes na arquitetura

--- database-interface/src/main/java/com/workers/orm/UserORM.java
+++ database-interface/src/main/java/com/workers/orm/UserORM.java
@@ -3,9 +3,15 @@ package com.workers.orm;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
+@Getter @Setter
 public class UserORM {
+    private Long id;
     private String email;
     private String password;
 }
--- database-interface/src/main/java/com/workers/queries/DBUser.java
+++ database-interface/src/main/java/com/workers/queries/DBUser.java
@@ -1,9 +1,10 @@
 package com.workers.queries;
 
 import com.workers.orm.UserORM;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface DBUser {
+@Repository
+public interface DBUser extends JpaRepository<UserORM, Long> {
     UserORM findByEmail(String email);
-
-    UserORM save(UserORM userORM);
 }
