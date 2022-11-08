9f93219c7f57a15412c56230195ba4154cd6028b
Moisés Souza <moi0983@gmail.com>
2022-11-08 11:16:13 -0300

    ajustes na arquitetura

--- database-interface/src/main/java/com/workers/orm/UserORM.java
+++ database-interface/src/main/java/com/workers/orm/UserORM.java
@@ -3,15 +3,21 @@ package com.workers.orm;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-
+import javax.persistence.*;
 @Entity
 @Table(name = "user")
 @Getter @Setter
 public class UserORM {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name="password", nullable = false)
     private String password;
+
 }
---
+++ web-application/src/main/java/com/workers/config/Config.java
@@ -0,0 +1,9 @@
+package com.workers.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class Config {
+}
