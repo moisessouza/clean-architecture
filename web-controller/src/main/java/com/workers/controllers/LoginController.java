1e24be54a88e65e986ce22427d820b5e6c59a80f
Moisés Souza <moi0983@gmail.com>
2022-11-10 13:20:57 -0300

    adicionando configuração para login

--- web-controller/src/main/java/com/workers/controllers/LoginController.java
+++ web-controller/src/main/java/com/workers/controllers/LoginController.java
@@ -2,7 +2,6 @@ package com.workers.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -12,8 +11,4 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String  login() {
-        return "home";
-    }
 }
