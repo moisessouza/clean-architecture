4a13f5a3080df91f28922df14c722847bbc8785b
Moisés Souza <moi0983@gmail.com>
2022-11-10 11:42:05 -0300

    adicionando configuração para login

--- web-controller/src/main/java/com/workers/config/WebSecurityConfig.java
+++ web-controller/src/main/java/com/workers/config/WebSecurityConfig.java
@@ -2,17 +2,23 @@ package com.workers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public WebSecurityConfig (UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,18 +33,22 @@ public class WebSecurityConfig {
                 )
                 .logout((logout) -> logout.permitAll());
 
+        http.authenticationProvider(authProvider());
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
---
+++ web-controller/src/main/java/com/workers/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.workers.userdetails;
+
+import com.workers.presenters.models.UserInputImpl;
+import com.workers.presenters.models.UserOutputImpl;
+import com.workers.usecase.UserService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    public UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserInputImpl impl = new UserInputImpl(username, null);
+        UserOutputImpl output = (UserOutputImpl ) userService.findByEmail(impl);
+
+        UserDetails user = User.withUsername(output.getEmail()).password(output.getPassword()).authorities("USER").build();
+
+        return user;
+
+    }
+}
