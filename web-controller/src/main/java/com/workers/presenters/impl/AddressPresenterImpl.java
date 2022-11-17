09265bb69e0922954811e127389e56bbd7d949f0
Moisés Souza <moi0983@gmail.com>
2022-11-17 11:57:12 -0300

    adicionando configuração para login

--- web-controller/src/main/java/com/workers/presenters/impl/AddressPresenterImpl.java
+++ web-controller/src/main/java/com/workers/presenters/impl/AddressPresenterImpl.java
@@ -4,6 +4,7 @@ import com.workers.entities.AddressEntity;
 import com.workers.presenters.AddressPresenter;
 import com.workers.presenters.models.address.AddressInput;
 import com.workers.presenters.models.address.AddressOutput;
+import com.workers.presenters.models.address.AddressOutputImpl;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
@@ -20,23 +21,97 @@ public class AddressPresenterImpl implements AddressPresenter {
 
     @Override
     public AddressOutput createError(AddressInput addressInput, String messageCode) {
-        return null;
+
+        AddressOutputImpl impl = new AddressOutputImpl();
+
+        impl.setError(true);
+        impl.setMessage(getMessage(messageCode));
+
+        impl.setCountry(addressInput.getCountry());
+        impl.setZipPostal(addressInput.getZipPostal());
+        impl.setState(addressInput.getState());
+        impl.setCity(addressInput.getCity());
+        impl.setNeighborhood(addressInput.getNeighborhood());
+        impl.setStreet(addressInput.getStreet());
+        impl.setNumber(addressInput.getNumber());
+        impl.setComplement(addressInput.getComplement());
+        impl.setUserEmail(addressInput.getUserEmail());
+
+        return impl;
     }
 
     @Override
     public AddressOutput createSuccess(AddressEntity entity, String messageCode) {
-        return null;
+
+        AddressOutputImpl impl = new AddressOutputImpl();
+
+        impl.setError(false);
+        impl.setMessage(getMessage(messageCode));
+
+        impl.setCountry(entity.getCountry());
+        impl.setZipPostal(entity.getZipPostal());
+        impl.setState(entity.getState());
+        impl.setCity(entity.getCity());
+        impl.setNeighborhood(entity.getNeighborhood());
+        impl.setStreet(entity.getStreet());
+        impl.setNumber(entity.getNumber());
+        impl.setComplement(entity.getComplement());
+        impl.setUserEmail(getUserEmail(entity));
+
+        return impl;
+
     }
 
     @Override
     public AddressOutput findByEmailError(String email, String messageCode) {
-        return null;
+
+        AddressOutputImpl impl = new AddressOutputImpl();
+
+        impl.setError(true);
+        impl.setMessage(getMessage(messageCode));
+
+        impl.setUserEmail(email);
+
+        return impl;
+
+
     }
 
     @Override
     public AddressOutput findByEmailSuccess(AddressEntity entity) {
-        return null;
+
+        AddressOutputImpl impl = new AddressOutputImpl();
+
+        impl.setError(false);
+
+        impl.setCountry(entity.getCountry());
+        impl.setZipPostal(entity.getZipPostal());
+        impl.setState(entity.getState());
+        impl.setCity(entity.getCity());
+        impl.setNeighborhood(entity.getNeighborhood());
+        impl.setStreet(entity.getStreet());
+        impl.setNumber(entity.getNumber());
+        impl.setComplement(entity.getComplement());
+        impl.setUserEmail(getUserEmail(entity));
+
+        return impl;
+
     }
+
+    private static String getUserEmail(AddressEntity entity) {
+
+        if (entity.getPersonalData() == null) {
+            return null;
+        }
+
+        if (entity.getPersonalData().getUser() == null) {
+            return null;
+        }
+
+        return entity.getPersonalData().getUser().getEmail();
+
+    }
+
 
     private String getMessage(String messageCode) {
         return messageSource.getMessage(messageCode, null,  Locale.getDefault());
--- web-controller/src/main/java/com/workers/presenters/models/address/AddressOutputImpl.java
+++ web-controller/src/main/java/com/workers/presenters/models/address/AddressOutputImpl.java
@@ -1,4 +1,26 @@
 package com.workers.presenters.models.address;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressOutputImpl implements AddressOutput {
+
+    private boolean error;
+    private String message;
+
+    private Long id;
+    private String country;
+    private String zipPostal;
+    private String state;
+    private String city;
+    private String neighborhood;
+    private String street;
+    private String number;
+    private String complement;
+    private String userEmail;
+
 }
